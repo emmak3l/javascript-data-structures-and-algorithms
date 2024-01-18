d9bde41b79d3b3c742cc8a0897831bc0a4225397
Emma <emma.a.kellam@gmail.com>
2024-01-18 16:38:22 -0500

    Completed frequency counter solution to areThereDuplicate exercise. I also included the instructors two pointer solution and one liner solution with a set for reference.

---
+++ javascript-data-structures-and-algorithms/frequency-counter-areThereDuplicates.js
@@ -0,0 +1,71 @@
+// Frequency Counter / Multiple Pointers - areThereDuplicates
+// Implement a function called, areThereDuplicates which accepts a variable number of arguments
+// and checks whether there are any duplicates among the arguments passed in.
+// You can solve this using the frequency counter pattern OR the multiple pointers pattern.
+
+// Examples:
+
+// areThereDuplicates(1, 2, 3) // false
+// areThereDuplicates(1, 2, 2) // true 
+// areThereDuplicates('a', 'b', 'c', 'a') // true 
+// Restrictions:
+
+// Time - O(n)
+
+// Space - O(n)
+
+// Bonus:
+
+// Time - O(n log n)
+
+// Space - O(1)
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// My Frequency Counter Solution:
+
+function areThereDuplicates(...args) {
+    let frequencyCounter = {};
+    for(let val of args){
+        frequencyCounter[val] = (frequencyCounter[val] || 0) + 1;
+    }
+    for(let key in frequencyCounter){
+        if(frequencyCounter[key] > 1){
+            return true;
+        };
+    }
+    return false;
+}
+
+console.log(areThereDuplicates(1, 2, 2))
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// ADDITIONAL INSTRUCTOR SOLUTIONS:
+
+
+// // areThereDuplicates Solution (Multiple Pointers)
+// function areThereDuplicates(...args) {
+//   args.sort((a, b) => {
+//     if (a < b) return -1;
+//     if (a > b) return 1;
+//     return 0;
+//   });
+ 
+//   let start = 0;
+//   let next = 1;
+//   while (next < args.length) {
+//     if (args[start] === args[next]) {
+//       return true;
+//     }
+//     start++;
+//     next++;
+//   }
+//   return false;
+// }
+
+
+// // areThereDuplicates One Liner Solution
+// function areThereDuplicates() {
+//   return new Set(arguments).size !== arguments.length;
+// }
