f82bfc6728a0fc9ba4202cd6a4e5dedb298d9402
Emma <emma.a.kellam@gmail.com>
2024-01-30 16:55:16 -0500

    Completed lecture notes comparing bubble, selection, and insertion sort.

---
+++ javascript-data-structures-and-algorithms/comparing-bubble-selection-and-insertion-sort.js
@@ -0,0 +1,25 @@
+// Comparing Bubble, Selection, and Insertion Sort Lecture
+
+// Big O of Sorting Algorithms
+
+/*
+   -----------------------------------------------------------------------------------------
+   |   Algorithm  | Time Complexity | Time Complexity | Time Complexity | Space Complexity |
+   |              |     (Best)      |    (Average)    |     (Worst)     |                  |
+   -----------------------------------------------------------------------------------------
+   |    Bubble    |      O(n)       |      O(n^2)     |      O(n^2)     |       O(1)       |
+   |     Sort     |                 |                 |                 |                  |
+   -----------------------------------------------------------------------------------------
+   |   Insertion  |      O(n)       |      O(n^2)     |      O(n^2)     |       O(1)       |
+   |     Sort     |                 |                 |                 |                  |
+   -----------------------------------------------------------------------------------------
+   |   Selection  |      O(n^2)     |      O(n^2)     |      O(n^2)     |       O(1)       |
+   |     Sort     |                 |                 |                 |                  |
+   -----------------------------------------------------------------------------------------
+*/
+
+// Recap
+// - Sorting is fundamental!
+// - Bubble sort, selection sort, and insertion sort are all roughly equivalent
+// - All have average time complexities that are quadratic
+// - We can do better... but we need more complex algorithms
